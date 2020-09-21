e80b021fe3e363e61ce63998118bcb4e66a4a296
Marco Massari Calderone <marco@marcomc.com>
2020-09-21 00:22:41 -0300

    rationalise the code and add support for parameters

--- export-apps-lists.sh
+++ export-apps-lists.sh
@@ -4,107 +4,235 @@
 # - Allow parameters to chose the Mackup storage engine/directory
 #
 #
+function setup_environment {
+  [[ -z $destination_dir ]] && destination_dir='.' # default destination if no '-d' is specified
+  homebrew_taps_list_file="${destination_dir}/homebrew_taps.txt"
+  homebrew_packages_list_file="${destination_dir}/homebrew_packages.txt"
+  homebrew_cask_apps_list_file="${destination_dir}/homebrew_cask_apps.txt"
+  mas_apps_list_file="${destination_dir}/mas_apps.txt"
+  npm_global_packages_list_file="${destination_dir}/npm_global_packages.json"
+  pip_packages_list_file="${destination_dir}/pip_packages.txt"
+  ruby_gems_list_file="${destination_dir}/ruby_gems.txt"
+  macprefs_backup_dir="$HOME/Library/Mobile Documents/com~apple~CloudDocs/Macprefs"
+}
 
-export_homebrew_packages=true
-export_homebrew_cask=true
-export_mas_apps=true
-export_npm_global_packages=true
-export_pip_packages=true
-mackup_backup=true
-macprefs_backup=true
-export_ruby_user_gems=true
+function export_homebrew_taps {
+  if command -v brew >/dev/null 2>&1; then
+    printf "Exporting Homebrew Taps list to %s..." "${homebrew_taps_list_file}"
+    brew tap-info --installed | grep -v -e '^$' | grep -v 'From\|files' | cut -d: -f1 > "${homebrew_taps_list_file}"
+    printf " done!\n"
+  else
+    printf "[Error] brew is not installed"
+    exit 1
+  fi
+}
 
+function export_homebrew_packages {
+  if command -v brew >/dev/null 2>&1; then
+    printf "Exporting Homebrew Packages list to %s..." "${homebrew_packages_list_file}"
+    brew list | grep '^[0-9]' -v > "${homebrew_packages_list_file}"
+    printf " done!\n"
+  else
+    printf "[Error] brew is not installed"
+    exit 1
+  fi
+}
 
-destination_dir="../files/lists"
-homebrew_taps_list_file="${destination_dir}/homebrew_taps.txt"
-homebrew_packages_list_file="${destination_dir}/homebrew_packages.txt"
-homebrew_cask_apps_list_file="${destination_dir}/homebrew_cask_apps.txt"
-mas_apps_list_file="${destination_dir}/mas_apps.txt"
-npm_global_packages_list_file="${destination_dir}/npm_global_packages.json"
-pip_packages_list_file="${destination_dir}/pip_packages.txt"
-ruby_gems_list_file="${destination_dir}/ruby_gems.txt"
-macprefs_backup_dir="$HOME/Library/Mobile Documents/com~apple~CloudDocs/Macprefs"
+function export_homebrew_cask {
+  if command -v brew >/dev/null 2>&1; then
+    printf "Exporting Homebrew Cask apps list to %s..." "${homebrew_cask_apps_list_file}"
+    brew list --cask | grep '^[0-9]' -v > "${homebrew_cask_apps_list_file}"
+    printf " done!\n"
+  else
+    printf "[Error] brew is not installed"
+    exit 1
+  fi
+}
 
-if "${export_homebrew_packages}" or "${export_homebrew_cask}"; then
-  printf "Exporting Homebrew Taps list to %s..." "${homebrew_taps_list_file}"
-  brew tap-info --installed | grep -v -e '^$' | grep -v 'From\|files' | cut -d: -f1 > "${homebrew_taps_list_file}"
-  printf " done!\n"
-fi
+function export_homebrew_all {
+  eval export_homebrew_taps
+  eval export_homebrew_packages
+  eval export_homebrew_cask
+}
 
-if "${export_homebrew_packages}"; then
-  printf "Exporting Homebrew Packages list to %s..." "${homebrew_packages_list_file}"
-  brew list | grep '^[0-9]' -v > "${homebrew_packages_list_file}"
-  printf " done!\n"
-fi
+function export_mas_apps {
+  if command -v mas; then
+    printf "Exporting MacAppStore apps list to %s..." "${mas_apps_list_file}"
+    mas list | sed 's/ /,/' > "${mas_apps_list_file}"
+    # returns list like:
+    # ID,Name (version)
+    # 402415186,GarageBuy (3.4)
+    # ...
+    printf " done!\n"
+  else
+    printf "[Error] mas is not installed"
+    exit 1
+  fi
+}
 
-if "${export_homebrew_cask}"; then
-  printf "Exporting Homebrew Cask apps list to %s..." ${homebrew_cask_apps_list_file}
-  brew list --cask | grep '^[0-9]' -v > "${homebrew_cask_apps_list_file}"
-  printf " done!\n"
-fi
+function export_npm_global_packages {
+  if command -v npm >/dev/null 2>&1; then
+    printf "Exporting NPM Global packages list to %s..." "${npm_global_packages_list_file}"
+    npm list -g --depth=0 --json > "${npm_global_packages_list_file}"
+    # returns list like:
+    # {
+    #   "dependencies": {
+    #     "gulp": {
+    #       "version": "4.0.2",
+    #       "from": "gulp@latest",
+    #       "resolved": "https://registry.npmjs.org/gulp/-/gulp-4.0.2.tgz"
+    #     },
+    #     ""yarn"": {
+    #       ...
+    #     }
+    #   }
+    # }
+    printf " done!\n"
+  else
+    printf "[Error] npm is not installed"
+    exit 1
+  fi
+}
 
-if "${export_mas_apps}"; then
-  printf "Exporting MacAppStore apps list to %s..." "${mas_apps_list_file}"
-  mas list | sed 's/ /,/' > "${mas_apps_list_file}"
-  # returns list like:
-  # ID,Name (version)
-  # 402415186,GarageBuy (3.4)
-  # ...
-  printf " done!\n"
-fi
+function export_pip_packages {
+  if command -v pip >/dev/null 2>&1; then
+    printf "Exporting PIP packages list to %s..." "${pip_packages_list_file}"
+    pip list -o --format freeze  2> /dev/null | sed 's/==/,/'  > "${pip_packages_list_file}"
+    # returns list like:
+    # package==version
+    # ...
+    printf " done!\n"
+  else
+    printf "[Error] pip is not installed"
+    exit 1
+  fi
+}
 
-if "${export_npm_global_packages}"; then
-  printf "Exporting NPM Global packages list to %s..." ${npm_global_packages_list_file}
-  npm list -g --depth=0 --json > "${npm_global_packages_list_file}"
-  # returns list like:
-  # {
-  #   "dependencies": {
-  #     "gulp": {
-  #       "version": "4.0.2",
-  #       "from": "gulp@latest",
-  #       "resolved": "https://registry.npmjs.org/gulp/-/gulp-4.0.2.tgz"
-  #     },
-  #     ""yarn"": {
-  #       ...
-  #     }
-  #   }
-  # }
-  printf " done!\n"
-fi
+function export_ruby_user_gems {
+  if command -v gem >/dev/null 2>&1; then
+    printf "Exporting Ruby gems list to %s..." "${ruby_gems_list_file}"
+    gem list | grep -v 'default' |  sed 's/[(,)]//g' | cut -d "," -f1 | sed 's/ /,/g'  > "${ruby_gems_list_file}"
+    # returns list like:
+    # gem,version
+    # ...
+    printf " done!\n"
+  else
+    printf "[Error] gem is not installed"
+    exit 1
+  fi
+}
 
-if "${export_pip_packages}"; then
-  printf "Exporting PIP packages list to %s..." "${pip_packages_list_file}"
-  pip list -o --format freeze  2> /dev/null | sed 's/==/,/'  > "${pip_packages_list_file}"
-  # returns list like:
-  # package==version
-  # ...
-  printf " done!\n"
-fi
-
-if "${export_ruby_user_gems}"; then
-  printf "Exporting Ruby gems list to %s..." "${ruby_gems_list_file}"
-  gem list | grep -v 'default' |  sed 's/[(,)]//g' | cut -d "," -f1 | sed 's/ /,/g'  > "${ruby_gems_list_file}"
-  # returns list like:
-  # gem,version
-  # ...
-  printf " done!\n"
-fi
-
-if "${mackup_backup}"; then
-  if command -v mackup; then
+function mackup_backup {
+  if command -v mackup >/dev/null 2>&1; then
     printf "Backing up dotfiles with mackup to %s..." "${pip_packages_list_file}"
     mackup backup -f
     # use the configuration in ~/.mackup.cfg
     printf " done!\n"
+  else
+    printf "[Error] Mackup is not installed"
+    exit 1
   fi
-fi
+}
 
-if "${macprefs_backup}"; then
-  if command -v macprefs; then
+function macprefs_backup {
+  if command -v macprefs >/dev/null 2>&1; then
     #  Any preferences Mackup backs up won't be backed up by Macprefs
     printf "Backing up System Preferences with macprefs to %s..." "${macprefs_backup_dir}"
     sudo macprefs_backup_dir="${macprefs_backup_dir}" macprefs backup
     # use the env value of macprefs_backup_dir as a backup dir
     printf " done!\n"
   fi
-fi
+}
+
+function export_all {
+  eval export_homebrew_all
+  eval export_mas_apps
+  eval export_npm_global_packages
+  eval export_pip_packages
+  eval export_ruby_user_gems
+  # eval mackup_backup
+  # eval macprefs_backup
+}
+
+function main {
+  case "$1" in
+    -d|--directory)
+      destination_dir="$2"
+      shift 2
+      ;;
+    -*)
+      echo "[Error] Invalid option: $1." 1>&2
+      exit 1
+      ;;
+  esac
+
+  objective="$1"; shift
+  case "$objective" in
+    brew|homebrew)
+      shift
+      brew_option=$1; # fetch the action's option
+      # Process package options
+      case $brew_option in
+        taps)
+          export_requested="export_homebrew_taps"
+          ;;
+        casks)
+          export_requested="export_homebrew_packages"
+          ;;
+        packages)
+          export_requested="export_homebrew_cask"
+          ;;
+        all|'')
+          export_requested="export_homebrew_all"
+          ;;
+        *)
+          echo "[Error] Incorrect option '$brew_option' for export objective'$objective' " 1>&2
+          eval show_usage 1>&2
+          exit 1
+          ;;
+        esac
+      ;;
+    npm)
+      export_requested="export_npm_global_packages"
+      ;;
+    pip)
+      export_requested="export_pip_packages"
+      ;;
+    ruby)
+      export_requested="export_ruby_user_gems"
+      ;;
+    mas)
+      export_requested="export_mas_apps"
+      ;;
+    macprefs)
+      # nothing to do for now
+      export_requested="macprefs_backup"
+      exit 1
+      ;;
+    mackup)
+      # nothing to do for now
+      export_requested="mackup_backup"
+      exit 1
+      ;;
+    all)
+      # nothing to do for now
+      export_requested="export_all"
+      ;;
+    '')
+      echo "[Error] Missing export objective" 1>&2
+      eval show_usage 1>&2
+      exit 1
+      ;;
+    *)
+      echo "[Error] Invalid export objective '$objective'" 1>&2
+      eval show_usage 1>&2
+      exit 1
+      ;;
+  esac
+
+  eval setup_environment
+  [[ -n $export_requested ]] && eval "$export_requested"
+  exit 0
+}
+
+main "$@"
