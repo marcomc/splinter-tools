140cbcc4b635787e7ca476573d6eba869330e427
Marco Massari Calderone <marco@marcomc.com>
2020-09-22 01:03:02 -0300

    add -d -n options (ready to be integrated with splinter)

--- recovery-key-cert-generator.sh
+++ recovery-key-cert-generator.sh
@@ -1,62 +1,93 @@
 #!/bin/bash
 
-case "${1}" in
-  '--help' )
-    printf "Usage: %s [ KeychainName ]\n" "${0}"
-    exit
-  ;;
-  '')
-    keychain_name="FileVaultMaster"
-    ;;
-  *)
-    keychain_name="${1}"
-  ;;
-esac
+function abs_path {
+  local file="$1"
+  local absolute_path=""
+  directory=$(dirname "$file")
+  # shellcheck disable=SC2001 # can't avoid the use of sed
+  directory=$(echo "$directory" | sed 's+~+'"${HOME}"'+g' )
+  absolute_path="$(cd "$directory"|| exit 1; pwd )"
+  echo "${absolute_path}/$(basename "$file")"
+}
 
-output_dir="files/recovery-key-cert"
-keychain_file="${output_dir}/${keychain_name}.keychain"
-keychain_secret_output="${output_dir}/${keychain_name}-keychain-password.txt"
-der_cert="${output_dir}/${keychain_name}.der.cer"
-keychain_password=""Request the secret u
+function setup_environment {
+  red="\e[31m"
+  green="\e[32m"
+  yellow="\e[33m"
+  cyan="\e[36m"
+  white="\e[39m"
 
-red="\e[31m"
-yellow="\e[33m"
-cyan="\e[36m"
-white="\e[39m"
+  [[ -z "$keychain_name" ]] && keychain_name='FileVaultMaster'
+  [[ -z "$destination_dir" ]] && destination_dir="$(pwd)/recovery-key-cert"
+  keychain_file="${destination_dir}/${keychain_name}.keychain"
+  keychain_secret_output="${destination_dir}/${keychain_name}-keychain-password.txt"
+  der_cert="${destination_dir}/${keychain_name}.der.cer"
+}
 
 function from_keychain_to_cert {
-  eval ask_for_private_key_secret
 
-  printf ">> %bCreate %s keychain%b\n" "${cyan}" "${keychain_file}" "${white}"
-  security create-filevaultmaster-keychain -p "${KEYCHAIN_PASSWORD}" "${keychain_file}"
-
-  printf ">> %bExport DER certificate %s from %s keychain%b\n" "${cyan}" "${der_cert}" "${keychain_file}" "${white}"
-  security export -k "${keychain_file}" -t certs -o "${der_cert}"
+  printf "%b>>>>>>>>>>%b Creating %s keychain\n" "$green" "$white" "$keychain_file"
+  security create-filevaultmaster-keychain -p "$KEYCHAIN_PASSWORD" "$keychain_file"
+  printf "%b>>>>>>>>>>%b Exporting DER certificate %s from %s keychain\n" "$green" "$white" "$der_cert" "$keychain_file"
+  security export -k "$keychain_file" -t certs -o "$der_cert"
 }
 
 function ask_for_private_key_secret {
-  if [ -z "${KEYCHAIN_PASSWORD}" ]; then
-        printf ">> %bRequest the secret used to encript %b%s%b\n" "${cyan}" "${yellow}" "${keychain_file}" "${white}"
-        read -r -p ">> Insert your chosen secret: " -s keychain_password
-        printf "\n"
-        export KEYCHAIN_PASSWORD="${keychain_password}"
-        printf ">> %bKeychain password saved in %s%b\n" "${red}" "${keychain_secret_output}" "${white}"
-        echo "${KEYCHAIN_PASSWORD}" > "${keychain_secret_output}"
-        chmod 0600 "${keychain_secret_output}"
-        printf ">> %bStore the keychain password in a safe place (i.e. Bitwarden, LastPass or 1Password)%b\n" "${yellow}" "${white}"
-        printf ">> %bthen delete the file %s%b\n" "${yellow}" "${keychain_secret_output}" "${white}"
+  local keychain_password=""
+  if [[ -z $KEYCHAIN_PASSWORD ]]; then
+    printf "%b>>>>>>>>>>%b Requesting the secret used to encript %s\n" "$green" "$white" "$keychain_file"
+    read -r -p ">>>>>>>>>> Insert your chosen secret: " -s keychain_password
+    printf "\n"
+    export KEYCHAIN_PASSWORD="$keychain_password"
+    printf "%b>>>>>>>>>>%b Keychain password saved in %s\n" "$cyan" "$white" "$keychain_secret_output"
+    echo "$KEYCHAIN_PASSWORD" > "$keychain_secret_output"
+    chmod 0600 "$keychain_secret_output"
+    printf "%b>>>>>>>>>> Store the keychain password in a safe place (i.e. Bitwarden, LastPass or 1Password)%b\n" "$yellow" "$white"
+    printf "%b>>>>>>>>>> then delete the file %s%b\n" "$yellow" "$keychain_secret_output" "$white"
   else
-        printf ">> %b'KEYCHAIN_PASSWORD' is already set%b\n" "${red}" "${white}"
+    printf "%b>>>>>>>>>>%b 'KEYCHAIN_PASSWORD' is already set\n" "$yellow" "$white"
   fi
 }
 
-[ ! -d "${output_dir}" ] &&  mkdir -p "${output_dir}"
-if [ -f "${keychain_file}" ];then
-  printf ">> %b%s keychain file already exists!%b\n" "${yellow}" "${keychain_file}" "${white}"
-  exit
-else
-  eval from_keychain_to_cert
-  printf ">> %bOpen %s keychain for review\n%b" "${cyan}" "${keychain_name}" "${white}"
-  open "${keychain_file}"
-fi
-open "${output_dir}"
+function main {
+  while getopts ":d:n:h" option; do
+    case "$option" in
+      d)
+        destination_dir="$(abs_path "$OPTARG")"
+        ;;
+      n)
+        keychain_name="${OPTARG}"
+        ;;
+      h)
+        printf "Usage: %s [ -d path/to/destination/dir ] [ -n KeychainName ]\n" "$0"
+        exit 0
+        ;;
+      \?)
+        echo ">>>>>>>>>> Error: Invalid Option '-$option'" 1>&2
+        eval show_usage 1>&2
+        exit 1
+        ;;
+      :)
+        echo ">>>>>>>>>> Error: Option '-$option' is missing an argument" 1>&2
+        eval show_usage 1>&2
+        exit 1
+        ;;
+    esac
+  done
+
+  eval setup_environment
+
+  [ ! -d "$destination_dir" ] &&  mkdir -p "$destination_dir"
+  if [ -f "$keychain_file" ];then
+    printf "%b>>>>>>>>>>%b The keychain file '%s' already exists!\n" "$yellow" "$white" "$keychain_file"
+    exit
+  else
+    eval ask_for_private_key_secret
+    eval from_keychain_to_cert
+    printf "%b>>>>>>>>>>%b Opening the keychain '%s' for review\n" "$green" "$white" "$keychain_name"
+    open "$keychain_file"
+  fi
+  open "$destination_dir"
+}
+
+main "$@"
